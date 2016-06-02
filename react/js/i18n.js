945fff58433363d6a4048c7d7fd939650f8a84dc
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-06-02 20:09:19 +0200

    moved react sample to prod

--- react/js/i18n.js
+++ react/js/i18n.js
@@ -7,10 +7,7 @@ window.locize.init({
     },
     backend: {
       referenceLng: 'en',
-      apiKey: 'd889c501-267f-4bd0-bb41-5a30408f26fc',
-      projectId: 'fbb8b12d-7ab6-4cc4-a4f9-8ac0199b0e16',
-      //version: 'staging',
-      loadPath: 'https://api-dev.locize.io/{{projectId}}/{{version}}/{{lng}}/{{ns}}',
-      addPath: 'https://api-dev.locize.io/missing/{{projectId}}/{{version}}/{{lng}}/{{ns}}'
+      // apiKey: '',
+      projectId: '58ae1c51-1119-4228-b159-fa699dff2e27'
     }
   });
